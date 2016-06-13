c0ea9437a855b1e9db2b885acf148b8bb59f967f
Jarkko Laaksonen <jarkkola@gmail.com>
2016-06-13 17:15:43 +0300

    basic search functionality added

--- phonebook.rb
+++ phonebook.rb
@@ -1,26 +1,41 @@
+# coding: utf-8
 require 'json'
 
 class PhoneBook
     def initialize()
-        @persons = {1=>{:name => "Test"}}
+        @persons = {1=>{"firstname" => "Matti", "lastname" => "Meikäläinen"}, 2 => {"firstname" => "Maija", "lastname"=>"Meikäläinen"}}
     end
 
-    def get_person(sid)
-        json = ""
-        id = sid.to_i
-        if ( id > 0 )
-            person = @persons.select { |kid,hash| kid == id }
-            json = JSON.generate(person[id]) unless person.length == 0
-        end
+    def get_person(id)
+        json = "{}"
+        json = JSON.generate(@persons[id.to_i]) if @persons.has_key?(id.to_i)
         json
     end
 
     def add_person(json)
-        person = JSON.parse(json)
-        return 0 unless check_mandatory_fields(person)
-        id = next_index
-        @persons[id]=person
+        id=0
+        begin
+            person = JSON.parse(json)
+            return 0 unless check_mandatory_fields(person)
+            id = next_index
+            @persons[id]=person
+        rescue
+        end
         id
+    end
+
+    def search_persons(criterias)
+        persons = @persons
+        criterias.each do |criteria|
+            persons = persons.select do |key,value|
+                found = value.has_key?(criteria[:name])
+                if (found )
+                    found = value[criteria[:name]] == criteria[:criteria]
+                end
+                found
+            end
+        end
+        JSON.generate(persons)
     end
 
     def next_index
--- server.rb
+++ server.rb
@@ -3,20 +3,26 @@ require_relative 'phonebook'
 
 phonebook = PhoneBook.new
 
+get '/phonebook/person/search/' do
+    return_json(phonebook.search_persons([]))
+end
+
+get '/phonebook/person/search/:criterias' do
+    criterias = parse_search_criterias(params["criterias"])
+    json = phonebook.search_persons(criterias)
+    return_json(json)
+end
+
 get '/phonebook/person/:id' do
     json=phonebook.get_person(params['id'])
-    body(json)
-    headers({"Content-Type" => " application/json"})
-    if json == ""
-        status(404)
-        body("{}")
-    end
+    return_json(json)
 end
 
 post '/phonebook/person/' do
     request.body.rewind
     json = request.body.read
     id = phonebook.add_person(json)
+    status(404) if id == 0
     body(id.to_s)
 end
 
@@ -27,4 +33,19 @@ end
 def page_missing
     status(404)
     body("Page not found!")
+end
+
+def return_json(json)
+    status = 200
+    status = 404 if json == "{}"
+    [status,{"Content-Type" => " application/json"},json]
+end
+
+def parse_search_criterias(criterias)
+    criterias.split(';').map do |criteria|
+        components = criteria.split('=',2)
+        search = {:name => components[0].downcase}
+        search[:criteria]=components[1] if ( components.length > 1 )
+        search
+    end
 end

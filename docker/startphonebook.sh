52b72a4351c6ff84c16875c641d18921f147e24b
Jarkko Laaksonen <jarkkola@gmail.com>
2016-06-20 16:48:41 +0300

    server to use http default port 80, when run in porduction. also to drop privileges from root to phonebook after port 80 is binded

--- docker/startphonebook.sh
+++ docker/startphonebook.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 trap "echo TRAPed signal" HUP INT QUIT TERM
 /etc/init.d/postgresql start
-cd /usr/lib/phonebook/ && su phonebook -c 'ruby server.rb'
+RACK_ENV="production"
+export RACK_ENV
+cd /usr/lib/phonebook/ &&  ruby server.rb
 
 echo "[hit enter key to exit] or run 'docker stop <container>'"
 read
--- server.rb
+++ server.rb
@@ -1,50 +1,68 @@
-require 'sinatra'
+require 'sinatra/base'
 require_relative 'phonebook'
 require_relative 'database'
 
-set :bind=>"0.0.0.0"
+class PhoneBookServer < Sinatra::Base
+    set(:bind=>"0.0.0.0")
+    set(:reload_templates=>false)
+    configure :production do
+        set(:port=>80)
+    end
 
-database = Database.new
-phonebook = PhoneBook.new(database)
-
-get '/phonebook/person/search/' do
-    return_json(phonebook.get_all_persons)
-end
-
-get '/phonebook/person/search/:criterias' do
-    criterias = parse_search_criterias(params["criterias"])
-    json = phonebook.search_persons(criterias)
-    return_json(json)
-end
-
-get '/phonebook/person/:id' do
-    json=phonebook.get_person(params['id'])
-    return_json(json)
-end
-
-post '/phonebook/person/' do
-    request.body.rewind
-    json = request.body.read
-    id = phonebook.add_person(json)
-    status(404) if id == 0
-    body(id.to_s)
-end
-
-get '/' do
-    send_file('public/index.html')
-end
-
-def return_json(json)
-    status = 200
-    status = 404 if (json == "{}" || json == "[]" || json == "")
-    [status,{"Content-Type" => " application/json"},json]
-end
-
-def parse_search_criterias(criterias)
-    criterias.split(';').map do |criteria|
-        components = criteria.split('=',2)
-        search = {:name => components[0].downcase}
-        search[:criteria]=components[1] if ( components.length > 1 )
-        search
+    def initialize()
+        if ( settings.production? && Process::Sys.getuid == 0)
+            uid = Etc.getpwnam("phonebook").uid
+            Process::Sys.setuid(uid)
+        end
+        database = Database.new
+        @phonebook = PhoneBook.new(database)
+    end
+    
+    get '/' do
+        send_file('public/index.html')
+    end
+    
+    get '/phonebook/person/search/' do
+        return_json(@phonebook.get_all_persons)
+    end
+    
+    get '/phonebook/person/search/:criterias' do
+        criterias = parse_search_criterias(params["criterias"])
+        json = @phonebook.search_persons(criterias)
+        return_json(json)
+    end
+    
+    get '/phonebook/person/:id' do
+        json=@phonebook.get_person(params['id'])
+        return_json(json)
+    end
+    
+    post '/phonebook/person/' do
+        request.body.rewind
+        json = request.body.read
+        id = @phonebook.add_person(json)
+        status(404) if id == 0
+        body(id.to_s)
+    end
+    
+    get '/' do
+        send_file('public/index.html')
+    end
+    
+    def return_json(json)
+        status = 200
+        status = 404 if (json == "{}" || json == "[]" || json == "")
+        [status,{"Content-Type" => " application/json"},json]
+    end
+    
+    def parse_search_criterias(criterias)
+        criterias.split(';').map do |criteria|
+            components = criteria.split('=',2)
+            search = {:name => components[0].downcase}
+            search[:criteria]=components[1] if ( components.length > 1 )
+            search
+        end
     end
 end
+
+PhoneBookServer.run!
